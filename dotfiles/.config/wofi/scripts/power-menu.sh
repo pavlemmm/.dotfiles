e3d105d51b086e342cca1767046d631daafa1a88
pavlemmm <pavlemitic03@gmail.com>
2025-08-08 20:55:32 +0200

    wofi power menu

---
+++ dotfiles/.config/wofi/scripts/power-menu.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+if pgrep -x wofi; then
+    killall wofi
+    exit
+fi
+
+
+compositor=$(echo $XDG_CURRENT_DESKTOP)
+
+case "$compositor" in
+    sway)
+        reload_cmd="swaymsg reload"
+        logout_cmd="swaymsg exit"
+        lock_cmd="swaylock"
+        ;;
+    hyprland)
+        reload_cmd="hyprctl reload"
+        logout_cmd="hyprctl dispatch exit"
+        lock_cmd="hyprlock"
+        ;;
+    *)
+        notify-send "Unsupported session" "Unknown compositor: $compositor"
+        exit 1
+        ;;
+esac
+
+
+option_1="1    Power Off"
+option_2="2    Restart"
+option_3="3    Reload $compositor"
+option_4="4    Lock"
+option_5="5    Log Out"
+
+option=$(printf "$option_1\n$option_2\n$option_3\n$option_4\n$option_5" | wofi --show dmenu --width 200 --height 210 --prompt "Power Menu")
+
+case $option in
+    $option_1) poweroff ;;
+    $option_2) reboot ;;
+    $option_3) $reload_cmd ;;
+    $option_4) $lock_cmd ;;
+    $option_5) $logout_cmd ;;
+    *) exit 1 ;;
+esac
